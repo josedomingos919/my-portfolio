76bd5d0e86204a7966606cd2da29d46c18ac0aa6
josedomingos919 <josedomingos919@gmail.com>
2025-10-31 13:07:44 +0100

    feat: create mobile nav

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,9 +1,37 @@
-import React from "react";
-import { FaCode } from "react-icons/fa";
+"use client";
 
-const Nav = () => {
+import Link from "next/link";
+
+import { useEffect, useState } from "react";
+import { FaCode } from "react-icons/fa";
+import { BiDownload } from "react-icons/bi";
+import { NavLinks } from "@/constant/constant";
+import { HiBars3BottomRight } from "react-icons/hi2";
+
+type Props = {
+  openNav: () => void;
+};
+
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      else setNavBg(false);
+    };
+
+    window.addEventListener("scroll", handler);
+
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
   return (
-    <div className="transition-all duration-200 h-[12vh] z-[1000] fixed w-full">
+    <div
+      className={`transition-all ${
+        navBg ? "bg-[#0f142ed9] shadow-md " : "fixed"
+      } duration-200 h-[12vh] z-[1000] fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] mx-auto">
         {/* LOGO */}
         <div className="flex items-center space-x-2">
@@ -13,6 +41,33 @@ const Nav = () => {
           <h1 className="text-xl hidden sm:block md:text-2xl text-white font-bold">
             JOSÉ
           </h1>
+        </div>
+        {/* nav links */}
+        <div className="hidden lg:flex items-center space-x-10">
+          {NavLinks.map((link) => {
+            return (
+              <Link
+                key={link.id}
+                href={link.url}
+                className="text-base font-medium text-white hover:text-cyan-300  transition-all duration-200"
+              >
+                <p>{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
+        {/* buttons */}
+        <div className="flex items-center space-x-4">
+          {/* CV Download */}
+          <button className="px-8 py-3.5 text-sm cursor-pointer rounded-lg bg-blue-800 hover:bg-blue-900 transition-all duration-300 text-white flex items-center space-x-2">
+            <BiDownload className="w-5 h-5" />
+            <span>DownLoad</span>
+          </button>
+          {/* Burger Menu */}
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-white lg:hidden"
+          />
         </div>
       </div>
     </div>
---
+++ constant/constant.ts
@@ -0,0 +1,24 @@
+export const NavLinks = [
+  { id: 1, url: "#", label: "Início" },
+  { id: 2, url: "#", label: "Serviços" },
+  {
+    id: 3,
+    url: "#",
+    label: "Currículo",
+  },
+  {
+    id: 4,
+    url: "#",
+    label: "Projetos",
+  },
+  {
+    id: 5,
+    url: "#",
+    label: "Habilidades",
+  },
+  {
+    id: 6,
+    url: "#",
+    label: "Contactos",
+  },
+];
